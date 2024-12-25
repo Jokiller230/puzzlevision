be96d3962fa565f5dd2cd87cf24f51c1168986e1
Jo <jo@thevoid.cafe>
2024-12-25 23:35:51 +0100

    feat(modules/nixos): deactivate Wi-Fi powersave feat(modules/home): add default user icon

--- modules/nixos/common/networking/default.nix
+++ modules/nixos/common/networking/default.nix
@@ -1,16 +1,21 @@
 {
   lib,
   namespace,
-
   config,
   ...
-}: with lib; with lib.${namespace};
+}:
 let
+  inherit (lib) mkIf mkEnableOption;
   cfg = config.${namespace}.common.networking;
 in {
-  options.${namespace}.common.networking = { enable = mkEnableOption "networking"; };
+  options.${namespace}.common.networking = {
+    enable = mkEnableOption "Whether to enable networking through NetworkManager.";
+  };
 
   config = mkIf cfg.enable {
-    networking.networkmanager.enable = true;
+    networking.networkmanager = {
+      enable = true;
+      wifi.powersave = false;
+    };
   };
 }
