6dc0e02a928fa33ef66bf045afd567e6c552f17f
Jo <jo@thevoid.cafe>
2025-05-14 08:25:41 +0200

    ✨ Add user activation script, which removes home manager backup files

---
+++ modules/nixos/services/home-manager/default.nix
@@ -0,0 +1,14 @@
+{...}: {
+  # Todo: rewrite as recursive operation, based on ${namespace}.users
+  system.userActivationScripts = {
+    removeConflictingHomeManagerBackups = {
+      text = ''
+        rm -f /home/jo/.gtkrc-2.0.homeManagerBackupFileExtension
+        rm -f /home/jo/.config/gtk-3.0/gtk.css.homeManagerBackupFileExtension
+        rm -f /home/jo/.config/gtk-4.0/gtk.css.homeManagerBackupFileExtension
+      '';
+    };
+  };
+
+  home-manager.backupFileExtension = "homeManagerBackupFileExtension";
+}
