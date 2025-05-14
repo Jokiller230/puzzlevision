2f5395dc64e7607fbaeca4cc85461ab3ad7c0c3a
Jo <jo@thevoid.cafe>
2025-05-14 08:07:40 +0200

    ✨ Add home packages for user `jo`

--- homes/x86_64-linux/jo/default.nix
+++ homes/x86_64-linux/jo/default.nix
@@ -4,8 +4,28 @@
   };
 
   home.packages = with pkgs; [
-    zed-editor
+    ## GENERAL
+    youtube-music
+    discord
+    ghostty
+    teams-for-linux
+    enpass
+
+    ## WEB
     firefox
+    ungoogled-chromium
+
+    ## EDITORS
+    nano
+    zed-editor
+    apostrophe
+    jetbrains.phpstorm
+    arduino-ide
+    obsidian
+
+    ## RUNTIMES and CLIs for development
+    bun
+    git
   ];
 
   home.stateVersion = "25.05";
