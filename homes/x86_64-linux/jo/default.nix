c48551f4bea59520440df2882a25c998dbaef874
Jo <reckers.johannes@proton.me>
2025-12-16 01:44:05 +0100

    ♻️ Move firefox user config into correct directory

--- homes/x86_64-linux/jo/default.nix
+++ homes/x86_64-linux/jo/default.nix
@@ -17,19 +17,6 @@
     themes.catppuccin.enable = true;
     apps = {
       zed.enable = true;
-      firefox = {
-        enable = true;
-        extensions = [
-          "uBlock0@raymondhill.net"
-          "ATBC@EasonWong"
-          "languagetool-webextension@languagetool.org"
-          "firefox-enpass@enpass.io"
-          "firefox@tampermonkey.net"
-          "wappalyzer@crunchlabz.com"
-          "{7a7a4a92-a2a0-41d1-9fd7-1e92480d612d}"
-          "{d49033ac-8969-488c-afb0-5cdb73957f41}"
-        ];
-      };
     };
 
     cli = {
