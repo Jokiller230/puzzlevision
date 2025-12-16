8a3989ce0b771d564d9cf309e5acff5f9414e944
Jo <reckers.johannes@proton.me>
2025-12-16 01:40:19 +0100

    ♻️ Move user-specific gnome config out of global context

--- homes/x86_64-linux/jo/desktop/gnome/default.nix
+++ homes/x86_64-linux/jo/desktop/gnome/default.nix
@@ -1,4 +1,32 @@
 { ... }:
 {
-  puzzlevision.desktop.gnome.wallpaper = ../wallpapers/macchiato-waves.jpg;
+  puzzlevision.desktop.gnome = {
+    favorite-apps = [
+      "org.gnome.Nautilus.desktop"
+      "obsidian.desktop"
+      "firefox.desktop"
+      "dev.zed.Zed.desktop"
+    ];
+
+    wallpaper = ../wallpapers/macchiato-waves.jpg;
+  };
+
+  dconf.settings = {
+    "org/gnome/shell/extensions/unite" = {
+      show-window-buttons = "never";
+      hide-window-titlebars = "maximized";
+
+      use-activities-text = false;
+      extend-left-box = false;
+      reduce-panel-spacing = false;
+      show-legacy-tray = false;
+      show-appmenu-button = false;
+      show-desktop-name = false;
+      enable-titlebar-actions = false;
+      restrict-to-primary-screen = false;
+      hide-activities-button = "never";
+      autofocus-windows = true;
+      notifications-position = "right";
+    };
+  };
 }
